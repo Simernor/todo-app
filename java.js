18efd83fcecbd96040d18f1457ef857aa683b6b8
Simernor <simonnorenius@gmail.com>
2022-12-08 09:11:06 +0100

    update

--- java.js
+++ java.js
@@ -1,27 +1,40 @@
 console.log("jscript-initialized");
 
-function addTodoItem(){
+function addInitialTodos(x, y){
+    addTodoItem(x, y);
 
+}
+
+function onformSubmit(){
+    const inputTitle = document.querySelector("input[name='todo-title']");
+    const inputText = document.querySelector("input[name='todo-text']");
+    addTodoItem(inputTitle.value, inputText.value);
+}
+
+function addTodoItem(title, text){
     const targetElement = document.getElementById("todo-container");
+    
     const newTodoItem = document.createElement("div");
-    newTodoItem.classList.add("todo-item");
-
     const newTodoTitle = document.createElement("h2");
-    newTodoTitle.innerText = "Todo title";
-    newTodoItem.appendChild(newTodoTitle);
-
+    const newTodoDate = document.createElement("span");
     const newTodoText = document.createElement("p");
-    newTodoText.innerText = "Something to do";
-    newTodoItem.appendChild(newTodoText);
-
     const newButtonRemove = document.createElement("button");
+
+    newTodoTitle.innerText = title;
+    newTodoDate.innerText = `${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()}`
+    newTodoText.innerText = text;
     newButtonRemove.innerText = "Remove";
+
+    const determineCardColor = `card-color-${Math.floor(Math.random() * 3 + 1)}`
+    newTodoItem.classList.add("todo-item", determineCardColor);
     newButtonRemove.classList.add("btn-remove-todo");
+
     newButtonRemove.addEventListener("click", function(){
         newTodoItem.remove();
     });
     newTodoItem.appendChild(newButtonRemove);
 
-    targetElement.appendChild(newTodoItem);
+    newTodoItem.append(newTodoTitle, newTodoText, newTodoDate, newButtonRemove)
 
+    targetElement.appendChild(newTodoItem);
 }
